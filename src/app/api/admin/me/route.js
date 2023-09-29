bcc5db6ee380a96ba354c05fc5e8405aafe571ba
Shashwat Yashaswi <shashwatyashaswi@gmail.com>
2023-09-29 15:02:13 +0530

    changed default page

--- src/app/api/admin/me/route.js
+++ src/app/api/admin/me/route.js
@@ -3,18 +3,28 @@ import { verify } from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 export async function GET() {
-  const cookieStore = cookies();
-  const token = cookieStore.get(COOKIE_NAME);
-  if (!token) {
-    return new NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
-  const { value } = token;
   try {
+    const cookieStore = cookies();
+    if (!cookieStore) {
+      return new NextResponse.json(
+        { message: "Unauthorized" },
+        { status: 401 }
+      );
+    }
+    const token = cookieStore.get(COOKIE_NAME);
+    if (!token) {
+      return new NextResponse.json(
+        { message: "Unauthorized" },
+        { status: 401 }
+      );
+    }
+    const { value } = token;
     const user = verify(value, process.env.TOKEN_SECRET || "123");
     return new Response(JSON.stringify(user), {
       status: 200,
     });
   } catch (error) {
+    console.error("error at me/route", error);
     return new NextResponse.json(
       { message: "Something went wrong" },
       { status: 400 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    (async () => {
+      router.push("/admin/ldaps");
+    })(),
+      [];
+  });
   return (
     <main>
-      <h1>Home - This page is not supposed to be used</h1>
+      <h1>Loading...</h1>
       {/* TEST */}
       {/* <ForgotPasswordForm /> */}
     </main>
