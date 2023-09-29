b124e420d3984c7fbe51502a62820f91347a315c
Shashwat Yashaswi <shashwatyashaswi@gmail.com>
2023-09-29 15:13:16 +0530

    Changed routing

--- src/app/page3.js
+++ src/app/page3.js
@@ -6,7 +6,7 @@ export default function Home() {
   const router = useRouter();
   useEffect(() => {
     (async () => {
-      router.push("/admin/ldaps");
+      router.push("/ldaps/admin");
     })(),
       [];
   });
